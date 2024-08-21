440fe87e33187a24abf8d4ed8c8ec6b934ce631c
Drazox <68930610+drazoxXD@users.noreply.github.com>
2024-08-21 17:46:31 +0200

    rewrite

---
+++ drazox_radio/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Hellslicer rewritten by Drazox'
+
+files {
+	'index.html'
+}
+
+ui_page 'index.html'
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'Config.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
